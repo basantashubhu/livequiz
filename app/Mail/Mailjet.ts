5015706e9bc0f5a480b1904c6d6ad84712bae5d5
basantashubhu <callmebasanta44@gmail.com>
2021-01-24 13:26:48 +0545

    uniq email reg

--- app/Mail/Mailjet.ts
+++ app/Mail/Mailjet.ts
@@ -6,6 +6,7 @@ class Mailjet {
 
     constructor() {
         this.mailjet = mailjet.connect('57281faeb8da33de0fef14df9765b77d', '20da7cbe1c3c8e090fd82802f3c367c2')
+            .post("send", { 'version': 'v3.1' })
     }
 
     subject(subject: string) {
@@ -13,7 +14,7 @@ class Mailjet {
     }
 
     send(to: { Name: string, Email: string }, html: string) {
-        return this.mailjet.post("send", { 'version': 'v3.1' })
+        return this.mailjet
             .request({
                 "Messages": [
                     {
@@ -32,36 +33,3 @@ class Mailjet {
 }
 
 export default new Mailjet
-
-
-/* 
-    const transporter = Transporter.create()
-
-    transporter.sendMail({
-        to: user.email,
-        from: Transporter.from(),
-        subject: 'Please confirm your email address',
-        html: `<p><strong>Thanks for signing up to Live Quiz, We are happy to have you.</strong></p>
-                <p>Please take a second to make sure we have your correct email address. <br/></p>
-                <p><a href="${this.BASE_URL}/email/confirm/${user.id}?email=${user.email}"><strong>Confirm your email address</strong></a><br/></p>
-                <p>Or try this code,</p>
-                <p>Code : ${code} <br/><br/></p>
-                <p>If you did not sign up for Live Quiz Account, you can safely ignore this email.</p>
-                <p>Yours truly, <br/>
-                Live Quiz Team <br/>
-                <a href="${this.BASE_URL}">${this.BASE_URL}</a> <br/>
-                The Drive to Develop</p>`
-
-    }).then(function (info: SentMessageInfo) {
-
-        const resetEmail = new ResetEmail({
-            messageId: info.messageId,
-            messageUrl: nodemailer.getTestMessageUrl(info)
-        })
-        resetEmail.save()
-        response.send(user)
-
-    }, function (err: any) {
-        response.status(500).send({ message: err.message })
-    }) 
-*/
--- routes/api/api_route_auth.js
+++ routes/api/api_route_auth.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const {Kernel} = require('../../app/Http/Controllers/Kernel/Kernel')
 const {body} = require('express-validator')
-const { User } = require('../../app/models/User')
-const {exists} = require('../../app/Validations/exists')
+const {exists, unique} = require('../../app/Validations/exists')
 
 router.post('/v1/user/register', [
     body('first_name').exists({checkFalsy : true}).withMessage('First name field is required'),
     body('last_name').exists({checkFalsy : true}).withMessage('Last name field is required'),
     body('email').normalizeEmail().isEmail().withMessage('Valid email address required'),
+    unique('email', 'User', 'email'),
     body('password').isLength({min:6}).withMessage('Password must contain at least 6 characters'),
 ], Kernel.map('ApiUserResourceController@register'))
 
